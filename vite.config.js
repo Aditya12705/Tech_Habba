d1207c1120cfa7bd674e4fa1040004307483aa7c
Aditya12705 <aditya.banerjee12705@gmail.com>
2025-11-15 04:58:20 +0530

    Fix base path to match correct repository name Tech_Habba

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/tra2.0/',
+  base: '/Tech_Habba/',
   plugins: [react()],
   server: {
     port: 3000,
